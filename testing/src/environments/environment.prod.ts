d6897dbcaa5218d84040e2b2a90006158511a524
Nishant Singh <nishantpratapsingh333@gmail.com>
2024-12-29 17:04:38 +0530

    Fix .gitattributes file

---
+++ testing/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://code-smith-backend.onrender.com',
+};
---
+++ testing/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://code-smith-backend.onrender.com',
+};
